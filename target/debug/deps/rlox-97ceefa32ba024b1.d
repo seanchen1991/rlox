e66c0f2c9c72da9574755234be39a809c6e3c1b5
Sean Chen <skypemaster007@gmail.com>
2020-05-02 11:33:23 -0700

    First commit; bytecode compiler

--- target/debug/deps/rlox-97ceefa32ba024b1.d
+++
@@ -1,5 +0,0 @@
-/Users/seanchen/Projects/rlox/target/debug/deps/rlox-97ceefa32ba024b1.rmeta: src/main.rs
-
-/Users/seanchen/Projects/rlox/target/debug/deps/rlox-97ceefa32ba024b1.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rlox-aff8474a0aca43da.d
+++
@@ -1,5 +0,0 @@
-/Users/seanchen/Projects/rlox/target/debug/deps/rlox-aff8474a0aca43da.rmeta: src/main.rs
-
-/Users/seanchen/Projects/rlox/target/debug/deps/rlox-aff8474a0aca43da.d: src/main.rs
-
-src/main.rs:
